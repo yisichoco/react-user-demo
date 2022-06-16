ca2574d971a2e05e99fb41ae499bd2c464661811
yisichoco <yisichoco@gmail.com>
2022-06-16 15:56:24 +0800

    A

--- react/src/pages/Memeber/Login.js
+++ react/src/pages/Memeber/Login.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import { useState } from 'react'
 
 function Login(props) {
+  const [userData, setUserData] = useState({
+    username: '',
+    password: '',
+  })
+
+  const handleChange = (e) => {
+    const newUserData = {
+      ...userData,
+      [e.target.name]: e.target.value,
+    }
+
+    setUserData(newUserData)
+  }
   return (
-    <div>Login</div>
+    <>
+      <h1>登入</h1>
+      <form>
+        <div>
+          <label>帳號</label>
+          <input
+            type="text"
+            name="username"
+            value={userData.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>密碼</label>
+          <input
+            type="text"
+            name="password"
+            value={userData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">送出</button>
+      </form>
+    </>
   )
 }
-
 
 export default Login
--- react/src/pages/Memeber/Profile.js
+++ react/src/pages/Memeber/Profile.js
@@ -1,7 +1,65 @@
-import React from 'react'
+import { useState } from 'react'
 
 function Profile(props) {
-  return <div>Profile</div>
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    username: '',
+    password: '',
+  })
+
+  const handleChange = (e) => {
+    const newUserData = {
+      ...userData,
+      [e.target.name]: e.target.value,
+    }
+
+    setUserData(newUserData)
+  }
+  return (
+    <>
+      <h1>個人資料</h1>
+      <form>
+        <div>
+          <label>姓名</label>
+          <input
+            type="text"
+            name="name"
+            value={userData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            value={userData.phone}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>帳號</label>
+          <input
+            type="text"
+            name="username"
+            value={userData.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>密碼</label>
+          <input
+            type="text"
+            name="password"
+            value={userData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">送出</button>
+      </form>
+    </>
+  )
 }
 
 export default Profile
